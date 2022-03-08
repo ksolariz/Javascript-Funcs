bf62db6203729c338303df4f305cb746151859aa
Kauê Soares <83794987+ksolariz@users.noreply.github.com>
2022-03-08 16:27:45 -0300

    Update validaSelectsComMesmoID.js

--- validaSelectsComMesmoID.js
+++ validaSelectsComMesmoID.js
@@ -9,3 +9,35 @@ for(i=0;i<x.length;i++){
 if(mensagem != ''){
    alert(mensagem)   
 }
+
+function AbrirRS() {
+			
+			sub = document.querySelectorAll("#txtSubAreaBiling")
+			areas = document.querySelectorAll("#slcIdAreaTableBiling")
+			mensagem = ''
+			for(i=0;i<areas.length;i++){
+			    console.log(areas[i].value)
+			    if(areas[i].value == 0){
+			        mensagem += `Selecione a Área - linha ${i+1}\n`
+			    }
+			}
+		
+			
+			for(i=0;i<sub.length;i++){
+			    console.log(sub[i].value)
+			    if(sub[i].value == 0){
+			        mensagem += `Selecione a Sub-Área - linha ${i+1}\n`
+			    }
+			}
+
+			if(mensagem != ''){
+			   alert(mensagem)   
+			}else{
+				document.getElementById("hdnAbrirRS").value = 1;
+				document.getElementById('formSolicitaDocs').submit();
+			}
+
+			
+
+	
+	</script>
